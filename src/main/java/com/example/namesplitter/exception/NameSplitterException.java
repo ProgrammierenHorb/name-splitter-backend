6fc224a7ccafb265ab5e3438a46c27ecf73da0b4
julian <julian_2002@gmx.net>
2024-05-02 13:48:30 +0200

    add exception

---
+++ src/main/java/com/example/namesplitter/exception/NameSplitterException.java
@@ -0,0 +1,7 @@
+package com.example.namesplitter.exception;
+
+public class NameSplitterException extends RuntimeException {
+    public NameSplitterException(String message) {
+        super(message);
+    }
+}
